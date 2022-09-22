c43f58a52ba82f0b249eb19cdc2f13da4f8861b8
Rafa prieto <rafaprieto063@gmail.com>
2022-09-21 20:52:23 -0500

    controller and routes examples

--- src/controllers/task.controllers.js
+++ src/controllers/task.controllers.js
@@ -1,4 +1,4 @@
-// create task example
+// Create task example
 const create = (req, res) => {
       const newTask = {
             id: req.body.id,
@@ -12,6 +12,43 @@ const create = (req, res) => {
       });
 }
 
+// Obtener todas las tareas
+const getAll = (req, res) => {
+      res.json({ message: 'Retornar todas las tareas' });
+}
+
+// Obtener tarea por id
+const getById = (req, res) => {
+      const id = req.params;
+      console.log(id);
+
+}
+
+// Editar
+const edit = (req, res) => {
+      // obtener el id
+      const taskId = req.params;
+      res.json({ message: `Editando esta tarea ${taskId}` });
+}
+
+// Eliminar
+const deleteTask = (req, res) => {
+      // obtener el id
+      const taskId = req.params;
+      res.json({ message: `Eliminando esta tarea ${taskId}` });
+}
+
+
+const test = (req, res) => {
+      const taskId = res.send(req.params);
+      console.log(taskId);
+}
+
 module.exports = {
-      create
+      create,
+      getAll,
+      getById,
+      edit,
+      deleteTask,
+      test
 }
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,10 +1,18 @@
 const { Router } = require('express');
-const { create } = require('../controllers/task.controllers')
+const { create, getAll, getById, edit, deleteTask, test } = require('../controllers/task.controllers')
 
 
 // initialize routes
 const router = Router();
 
 router.post('/api/task/create', create);
+
+router.get('/api/task/tasks', getAll);
+
+router.get('/api/task/:id/getTask', getById);
+
+router.put('/api/task/edit', edit);
+
+router.delete('/api/task/delete', deleteTask);
 
 module.exports = router;
